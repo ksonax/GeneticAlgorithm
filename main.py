4012baf8a1b6a391700c0240bbea91ce2791ac2e
Kamil Niemiec <ksonax@gmail.com>
2022-11-11 20:58:19 +0100

    Created main User Interface

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-from UI.UserInterface import user_interface
+from UI.UserInterface import App
+import os
+import sys
 
-user_interface()
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
